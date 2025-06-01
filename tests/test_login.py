34a57b44a887a7270c1f1be09799e3a938430273
GontaaD <gontadenis2006@gmail.com>
2025-06-01 11:58:00 +0300

    робочий тест Wi-Fi

--- tests/test_login.py
+++ tests/test_login.py
@@ -21,21 +21,36 @@ def test_login_by_phone(browser):
     page.wait_for_selector('button.button.button--navy.button--small.user-login__button')
     page.click('button.button.button--navy.button--small.user-login__button')
 
-    page.wait_for_timeout(500)
     #Закрити бічне меню
-    page.click('button.close.ms-2')
+    #page.click('button.close.ms-2')
 
-    page.wait_for_timeout(500)
+    page.wait_for_timeout(2000)
     #Введення номеру
-#
-    phone_input = page.wait_for_selector('input.LeAKF')
-    phone_input.click()
-    phone_input.type('997952094')
-#
-    page.wait_for_timeout(1000)
+    # Дочекайся завантаження сторінки
+    page.wait_for_load_state("domcontentloaded")
 
-    # Натиснути кнопку "Отримати код" (перевірити селектор)
-    page.click('button._00LHV.fsoe1.NQrpD.fAKin')
+    # Виведи всі фрейми — це ключовий дебаг
+    for f in page.frames:
+        print("FRAME:", f.name, f.url)
+
+    # Знайди фрейм, де є поле
+    frame = None
+    for f in page.frames:
+        if f.locator('//input[@data-qaid="phone"]').count() > 0:
+            frame = f
+            break
+
+    if frame:
+        frame.fill('//input[@data-qaid="phone"]', '997952094')
+
+        button = frame.locator('button._00LHV.fsoe1.NQrpD.fAKin')
+        button.wait_for(state='visible')
+        button.click()
+        locator = page.locator('//input[@data-qaid="code-0"]')
+        locator.wait_for(state='visible')
+        locator.click()
+    else:
+        raise Exception("Фрейм із полем не знайдено!")
 
     # Можна додати паузу для ручного введення коду (опціонально)
     page.wait_for_timeout(20000)  # пауза
